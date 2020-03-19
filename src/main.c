a678294e7d9410db1a75432477f74b71b86d4356
Jan Horacek <me@apophis.cz>
2020-03-19 16:54:12 +0100

    Add timer0 @ 1000 Hz (1 ms).

--- src/main.c
+++ src/main.c
@@ -30,8 +30,14 @@ void init() {
 	leds_init();
 	buttons_init();
 	btn_on_pressed = button_pressed;
-	uart_init();
+	uart_init(25);
 	uart_on_receive = uart_received;
+
+	// Setup main timer0 on 1 ms
+	TCCR0A |= 1 << WGM01; // CTC mode
+	TCCR0B |= 0x03; // prescaler 64×
+	TIMSK0 |= 1 << OCIE0A; // enable interrupt on compare match A
+	OCR0A = 114; // set compare match A to match 1 ms
 
 	sei(); // enable interrupts globally
 }
@@ -41,4 +47,9 @@ void button_pressed(uint8_t button) {
 }
 
 void uart_received(uint8_t recipient, uint8_t *data, uint8_t size) {
+}
+
+ISR(TIMER0_COMPA_vect) {
+	// Timer0 on 1 ms
+	btn_update();
 }
