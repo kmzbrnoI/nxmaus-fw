8a4f5fdc57e582b7cbd2fb6e7473c5c23a2d671d
Jan Horacek <me@apophis.cz>
2020-03-19 12:05:05 +0100

    Add LEDs control.

---
+++ src/hardware.h
@@ -0,0 +1,18 @@
+#ifndef _HARDWARE_H_
+#define _HARDWARE_H_
+
+#include <avr/io.h>
+
+static inline void leds_init() {
+	DDRB |= 0x07; // output
+	PORTB &= 0xF8; // all LED outputs low
+}
+
+static inline void buttons_init() {
+}
+
+static inline void led_gr_left_on() { PORTB |= (1 << PORTB2); }
+static inline void led_gr_left_off() { PORTB &= ~(1 << PORTB2); }
+static inline void led_gr_left_toggle() { PORTB ^= (1 << PORTB2); }
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,6 +1,10 @@
 #include <stdbool.h>
+#include <util/delay.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
+
+#include "hardware.h"
+#include "lib/uart.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -11,9 +15,17 @@ void init();
 
 int main() {
 	init();
+
 	while (true) {
+		led_gr_left_toggle();
+		_delay_ms(250);
 	}
 }
 
 void init() {
+	leds_init();
+	buttons_init();
+	uart_init();
+
+	sei(); // enable interrupts globally
 }
