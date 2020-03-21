3a28181755209bb1fedbb7f4ffe3e329e28fdf50
Jan Horacek <me@apophis.cz>
2020-03-21 21:17:01 +0100

    Flash LEDs on init.

--- src/main.c
+++ src/main.c
@@ -23,8 +23,6 @@ int main() {
 	init();
 
 	while (true) {
-		led_gr_left_toggle();
-		_delay_ms(250);
 	}
 }
 
@@ -42,6 +40,16 @@ void init() {
 	TCCR0B |= 0x03; // prescaler 64×
 	TIMSK0 |= 1 << OCIE0A; // enable interrupt on compare match A
 	OCR0A = 114; // set compare match A to match 1 ms
+
+	led_gr_left_on();
+	led_gr_right_on();
+	led_red_on();
+
+	_delay_ms(250);
+
+	led_gr_left_off();
+	led_gr_right_off();
+	led_red_off();
 
 	sei(); // enable interrupts globally
 }
