6a47176ef7735b7f2dad8c09ed7e0bc2cb2e6666
Jan Horacek <me@apophis.cz>
2020-03-23 20:45:39 +0100

    Show loco direction properly.

--- src/state.c
+++ src/state.c
@@ -9,12 +9,19 @@ uint8_t state = ST_XN_UNADDRESSED;
 static inline void _show_blinking(uint16_t counter);
 static inline void _show_loco_direction(uint16_t counter);
 static inline void _show_loco_direction_blinking(uint16_t counter);
+static inline void _show_loco_released(uint16_t counter);
 
 ///////////////////////////////////////////////////////////////////////////////
 
 void state_show(uint16_t counter) {
 	if (state == ST_XN_UNADDRESSED || state == ST_CS_STATUS_ASKING || state == ST_LOCO_STATUS_ASKING) {
 		_show_blinking(counter);
+	} else if (state == ST_LOCO_MINE) {
+		_show_loco_direction(counter);
+	} else if (state == ST_LOCO_STOLEN) {
+		_show_loco_direction_blinking(counter);
+	} else if (state == ST_LOCO_RELEASED) {
+		_show_loco_released(counter);
 	}
 }
 
@@ -32,7 +39,17 @@ static inline void _show_blinking(uint16_t counter) {
 }
 
 static inline void _show_loco_direction(uint16_t counter) {
+	if (loco.forward) {
+		led_gr_left_off();
+		led_gr_right_on();
+	} else {
+		led_gr_left_on();
+		led_gr_right_off();
+	}
 }
 
 static inline void _show_loco_direction_blinking(uint16_t counter) {
+}
+
+static inline void _show_loco_released(uint16_t counter) {
 }
