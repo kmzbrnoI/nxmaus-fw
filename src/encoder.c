4f81b04662c74f735a40cfe38d8fd6aa29c2fda2
Jan Horacek <me@apophis.cz>
2020-03-24 16:01:51 +0100

    Just one encoder hit on one rotary step.

--- src/encoder.c
+++ src/encoder.c
@@ -12,9 +12,6 @@ uint8_t a_counter = ENCODER_THRESHOLD, b_counter = ENCODER_THRESHOLD;
 bool a_pressed = true, b_pressed = true;
 
 static inline void _a_pressed();
-static inline void _a_depressed();
-static inline void _b_pressed();
-static inline void _b_depressed();
 static inline void _callback(uint8_t val);
 
 void encoder_update() {
@@ -29,36 +26,27 @@ void encoder_update() {
 	} else {
 		if (a_counter > 0) {
 			a_counter--;
-			if (a_counter == 0) {
+			if (a_counter == 0)
 				a_pressed = false;
-				_a_depressed();
-			}
 		}
 	}
 
 	if (encoder_raw_b()) {
 		if (b_counter < ENCODER_THRESHOLD) {
 			b_counter++;
-			if (b_counter == ENCODER_THRESHOLD) {
+			if (b_counter == ENCODER_THRESHOLD)
 				b_pressed = true;
-				_b_pressed();
-			}
 		}
 	} else {
 		if (b_counter > 0) {
 			b_counter--;
-			if (b_counter == 0) {
+			if (b_counter == 0)
 				b_pressed = false;
-				_b_depressed();
-			}
 		}
 	}
 }
 
 static inline void _a_pressed() { _callback(b_pressed ? 0 : 1); }
-static inline void _a_depressed() { _callback(b_pressed ? 1 : 0); }
-static inline void _b_pressed() { _callback(a_pressed ? 1 : 0); }
-static inline void _b_depressed() { _callback(a_pressed ? 0 : 1); }
 
 static inline void _callback(uint8_t val) {
 	if (encoder_on_change != NULL)
